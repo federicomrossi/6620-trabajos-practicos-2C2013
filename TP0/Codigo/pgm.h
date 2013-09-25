ed417d76f69d6f76e38ba6215869823441ba4b86
pablisho <rodriguez.pabloariel@gmail.com>
2013-09-25 02:11:49 -0300

    Agregado comments

--- TP0/Codigo/pgm.h
+++ TP0/Codigo/pgm.h
@@ -2,6 +2,9 @@
 #define PGP_H_INCLUDED
 #include <stdio.h>
 
+/**
+*   x: ancho, y: alto, fileName: Nombre del archivo a grabar
+*/
 void pgm(unsigned x,unsigned y, char* fileName);
 
 #endif // PGP_H_INCLUDED
