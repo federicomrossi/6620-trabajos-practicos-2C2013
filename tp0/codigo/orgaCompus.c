36efc5bc62fd2a9183b5cc5072a0533e7a098bd0
Belen Beltran <belubeltran@gmail.com>
2013-09-27 20:54:55 -0300

    Acepta solo opciones de una sola letra como '-h' y no '-help'. Si hay algun error, muestra el menu de Help.

--- tp0/codigo/orgaCompus.c
+++ tp0/codigo/orgaCompus.c
@@ -1,10 +1,11 @@
 /*
  ============================================================================
- Name        : OrgaCompus.c
- Author      : Belen Beltran
- Version     :
- Copyright   :
- Description : Hello World in C, Ansi-style
+ Name         : OrgaCompus.c
+ Authors      : Belen Beltran (91718)
+ 	 	 	 	Federico Martin Rossi ()
+ 	 	 	 	Pablo Rodriguez ()
+ Version      : 1.0
+ Description  : Programa de dibujo de tableros de Ajedrez en formato PGM
  ============================================================================
  */
 
@@ -15,12 +16,11 @@
 #include <stdlib.h>
 #include "pgm.h"
 
-#define MAX_NAME 256
 
 //* Funciones Auxiliares *//
 
 void displayHelp() {
-	printf("%s", "Usage:\n \
+	printf("%s", "<- Help display ->\nUsage:\n \
 	tp0 -h\n\
 	tp0 -V\n\
 	tp0 [options]\n\
@@ -31,11 +31,11 @@ void displayHelp() {
 	-o, --output Path to output file.\n\
 	\nExamples:\n\
 	tp0 -o output.pgm\n\
-	tp0 -r 800x600 -o file.pgm \n");
+	tp0 -r 800x600 -o file.pgm \n<- End of help display ->\n");
 }
 
 void displayVersion() {
-	printf("%s", "Programa de dibujo de tableros de Ajedrez en formato PGM \
+	printf("%s", "This program draws chess boards in PGM file format\
 	\nVersion: v1.0\n");
 }
 
@@ -48,39 +48,47 @@ void getXY(char* optarg, unsigned* x, unsigned* y) {
 	}
 }
 
-void getFileName(char* optarg, char** fileName) {
-	strcpy(*fileName, optarg);
-}
-
 int main(int argc, char* argv[]) {
-	int opt, finished = 0;
-	char* file=NULL;
+	int opt, finished = 0, i = 1;
+	char* file = NULL;
 	unsigned x = 8, y = 8; // Valores default
 
-	while (((opt = getopt(argc, argv, "hVr:o:")) != -1) && finished == 0) {
-		switch (opt) {
-			// Help
-			case 'h': 	displayHelp();
-						finished = 1;
-						break;
-			// Version
-			case 'V':	displayVersion();
-						finished = 1;
-						break;
-			// Resolution
-			case 'r':	getXY(optarg, &x, &y);
-						break;
-			// Output
-			case 'o':   file=optarg;
-						printf("x: %d \ny: %d \nNombre Archivo: %s\n", x,
-								y, optarg);
-						finished = 1;
-						break;
+	while (((opt = getopt(argc, argv, "hVr:o:")) != -1) && !finished) {
+		// Solo ejecuta opcion si esta es de una sola letra
+		// Ej: ejecuta '-h', pero no '-help'
+		if (argv[i][2] == 0) {
+			i += 2;
+			switch (opt) {
+				// Help
+				case 'h': 	displayHelp();
+							finished = 1;
+							break;
+				// Version
+				case 'V':	displayVersion();
+							finished = 1;
+							break;
+				// Resolution
+				case 'r':	getXY(optarg, &x, &y);
+							break;
+				// Output
+				case 'o':   file = optarg;
+							finished = 1;
+							break;
+				// Ninguna de las anteriores
+				default:	finished = 1;
+							printf("Error: Unknown option selected.\n");
+							displayHelp();
+							break;
+			}
+		}
+		else {
+			finished = 1;
+			printf("Error: Unknown option selected.\n");
+			displayHelp();
 		}
 	}
-	if(file)
+	if (file)
         pgm(x, y, file);
-
 
 	return EXIT_SUCCESS;
 }
