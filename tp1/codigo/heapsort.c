4c9f7330264cb80b2bdc51a286392634ebbff2e0
Federico Rossi <federicomrossi@gmail.com>
2013-10-12 15:09:50 -0300

    TP1: Subida inicial de archivos

---
+++ tp1/codigo/heapsort.c
@@ -0,0 +1,23 @@
+/* ****************************************************************************
+ * ****************************************************************************
+ *
+ * LIBRERIA HEAPSORT
+ *
+ * ****************************************************************************
+ * ***************************************************************************/
+
+
+#include "heapsort.h"
+#include <string.h>
+
+
+
+// Funcion que aplica el algoritmo de ordenamiento Heapsort para ordenar un
+// arreglo de palabras.
+// PRE: 'words' es un puntero a un arreglo de punteros a caracter; 'arraysize'
+// es el tamanio de dicho arreglo.
+// POST: el arreglo 'words' queda ordenado.
+void heapsort(char* words[], int arraysize)
+{
+	
+}
---
+++ tp1/codigo/heapsort.h
@@ -0,0 +1,25 @@
+/* ****************************************************************************
+ * ****************************************************************************
+ *
+ * LIBRERIA HEAPSORT
+ *
+ * ****************************************************************************
+ * ***************************************************************************/
+
+
+
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+
+
+// Funcion que aplica el algoritmo de ordenamiento Heapsort para ordenar un
+// arreglo de palabras.
+// PRE: 'words' es un puntero a un arreglo de punteros a caracter; 'arraysize'
+// es el tamanio de dicho arreglo.
+// POST: el arreglo 'words' queda ordenado.
+void heapsort(char* words[], int arraysize);
+
+
+
+#endif
