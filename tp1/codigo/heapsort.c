40ad221440a276cc8f19e204fbad10f1a36af52c
pablisho <rodriguez.pabloariel@gmail.com>
2013-10-19 19:45:43 -0300

    Implementacion de HeapSort en C. PD: Chupenla.

--- tp1/codigo/heapsort.c
+++ tp1/codigo/heapsort.c
@@ -11,13 +11,50 @@
 #include <string.h>
 
 
+void heap(char* words[],int raiz, int fin){
+    char finished = 0;
+    char* tmp;
+    int hijo;
+    while (raiz*2+1 <= fin && !finished){
+        //Elijo el hijo mayor
+        if(raiz*2+1==fin || strcmp(words[raiz*2+1],words[raiz*2+2]) > 0 ){
+            hijo=raiz*2+1;
+        }else{
+            hijo=raiz*2+2;
+        }
+        //Checkeo si es mayor e intercambio
+        if(strcmp(words[raiz],words[hijo]) < 0){
+            tmp=words[raiz];
+            words[raiz]=words[hijo];
+            words[hijo]=tmp;
+            raiz=hijo;
+        }else{
+            finished=1;
+        }
+    }
+}
+
+
 
 // Funcion que aplica el algoritmo de ordenamiento Heapsort para ordenar un
 // arreglo de palabras.
 // PRE: 'words' es un puntero a un arreglo de punteros a caracter; 'arraysize'
 // es el tamanio de dicho arreglo.
 // POST: el arreglo 'words' queda ordenado.
-void heapsort(char* words[], int arraysize)
-{
-	
+void heapsort(char* words[], int arraysize){
+    int i;
+    char* tmp;
+    //Armo Heap
+    for (i=(arraysize/2)-1;i>=0;i--){
+        heap(words,i,arraysize);
+    }
+
+    //Extraigo raiz y armo heap
+    for(i=arraysize-1;i>0;i--){
+        tmp=words[0];
+        words[0]=words[i];
+        words[i]=tmp;
+        heap(words,0,i-1);
+    }
 }
+
