5986fb16667e8a3990c413c52f802faf6397bc65
pablisho <rodriguez.pabloariel@gmail.com>
2013-10-20 20:31:15 -0300

    Funcion heap andando!

--- tp1/codigo/heap.s
+++ tp1/codigo/heap.s
@@ -3,18 +3,16 @@
 .globl heap
 
 heap: 
-	subu $sp, $sp, 40 #creo el SRA 16 y LTA 16 y ABA 8 = 40
+	subu $sp, $sp, 40 #creo el SRA 16 y LTA 8 y ABA 16 = 40
 	sw $ra, 32($sp)
-	sw $sp, 28($sp)
-	sw $fp, 24($sp)
+	sw $fp, 28($sp)
+	sw $gp, 24($sp)
 	move $fp, $sp
 	sw $a0, 40($sp) #Guardo los parametros en el ABA
 	sw $a1, 44($sp)
-	sw $a2, 48($sp)
-	li $t0, 0 #t0, sp+8 = finished
-	sw $t0, 8($sp) 
-	#t1, sp+12  = tmp
-	#t2, sp+16  = hijo  
+	sw $a2, 48($sp) 
+	#t1, sp+16  = tmp
+	#t2, sp+20  = hijo  
 loop:	#comparacion de while. No hago la comp de finished, Break instead
 	sll $t3, $a1, 1 #raiz*2
 	addi $t3, $t3, 1 #+1
@@ -32,7 +30,8 @@ loop:	#comparacion de while. No hago la comp de finished, Break instead
 	jal strcmp
 	#recupero los temp necesarios
 	lw $a1, 44($sp)
-	blt $v0, $0, if
+	lw $a0, 40($sp)
+	bgt $v0, $0, if
 	#hago el else
 	sll $t2, $a1, 1
 	addiu $t2, $t2, 2
@@ -42,7 +41,7 @@ if:	#hago el if
 	addi $t2, $t2, 1	
 fi:	#Guardo los tmp necesarios
 	sw $t2, 16($sp)
-	#cargo los parametros
+	#cargo los parametrosi
 	sll $t2, $t2, 2
 	sll $a1, $a1, 2
 	addu $t3, $a0, $t2
@@ -51,7 +50,7 @@ fi:	#Guardo los tmp necesarios
 	lw $a1, 0($t3) 
 	#llamo a strcmp
 	jal strcmp
-	bgt $v0, $0, if2
+	blt $v0, $0, if2
 	#recupero los tmp necesarios. No hay nada!
 	#hago el else
 	j fin_loop
@@ -63,10 +62,10 @@ if2:	#hago el if
 	lw $t2, 16($sp)
 	#hago los intercambios
 	sll $t3, $a1, 2 #Index raiz x4 para array
-	addu $t3, $a0, $t3
-	lw $t1, 0($t3) #guardo en tmp el valor de words[raiz]
 	sll $t4, $t2, 2 #Index hijo x4 para array
+	addu $t3, $a0, $t3
 	addu $t4, $a0, $t4
+	lw $t1, 0($t3) # cargo el valor de words[raiz]
 	lw $t5, 0($t4) # cargo el valor de words[hijo]
 	sw $t5, 0($t3)	#guardo en words[raiz] = words[hijo]
 	sw $t1, 0($t4) #guardo en words[hijo] = tmp
@@ -75,9 +74,12 @@ if2:	#hago el if
 	j loop
 fin_loop:
 	#destruyo el stack
+	lw $a0, 40($sp)
+	lw $a1, 44($sp)
+	lw $a2, 48($sp)
 	lw $ra, 32($sp)
 	lw $fp, 28($sp)
 	lw $gp, 24($sp)
-	addiu $sp, $sp, 32
+	addiu $sp, $sp, 40
 	j $ra
 	
